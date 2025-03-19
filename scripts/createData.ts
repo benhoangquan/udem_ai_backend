61be50a5c2dd68be9bd37b942303153d3dcb7e18
benhoangquan <48300966+benhoangquan@users.noreply.github.com>
2025-03-19 10:11:58 -0400

    hotfix createData

--- scripts/createData.ts
+++ scripts/createData.ts
@@ -1,11 +1,8 @@
 import { getCliClient } from "sanity/cli"
 import createActivityData from "./createActivityData"
-import createEventData from "./createEventData"
 import createGeneralInfoData from "./createGeneralInfoData"
 import createMemberData from "./createMemberData"
-import createProjectData from "./createProjectData"
 import createResourceData from "./createResourceData"
-import createToolData from "./createToolData"
 import createPostData from "./createPostData"
 
 const COUNT = 1
@@ -19,12 +16,9 @@ async function createData() {
 
   for (let i = 0; i < COUNT; i++) {
     await createActivityData()
-    await createEventData()
     await createGeneralInfoData()
     await createMemberData()
-    await createProjectData()
     await createResourceData()
-    await createToolData()
     await createPostData()
   }
 }
