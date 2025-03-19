8db9c621b749cab74bd972964024af19909334e5
benhoangquan <48300966+benhoangquan@users.noreply.github.com>
2025-03-19 10:44:31 -0400

    Update Sanity package to version 3.80.0, simplify plugin configuration in sanity.config.ts, and remove unused validation step from CI workflows. Delete obsolete sanity_deploy.yaml workflow.

--- sanity.config.ts
+++ sanity.config.ts
@@ -14,34 +14,9 @@ export default defineConfig({
 
   basePath: '/studio',
 
-  plugins: [
-    structureTool({
-      structure: (S) =>
-        S.list()
-          .title('Content')
-          .items([
-            // Add your custom structure here
-            ...schemaTypes.map((type) => S.listItem().title(type.title).child(S.document().schemaType(type.name))),
-          ]),
-    }),
-    visionTool(),
-  ],
+  plugins: [structureTool(), visionTool()],
 
   schema: {
     types: schemaTypes,
-  },
-
-  document: {
-    // Prevent new documents from being created if the document is already published
-    productionUrl: async (prev, context) => {
-      const {slug, _type} = context.document
-      if (!slug || typeof slug !== 'object' || !('current' in slug) || typeof slug.current !== 'string') return prev
-
-      const url = new URL('/api/preview', location.origin)
-      url.searchParams.set('type', _type)
-      url.searchParams.set('slug', slug.current)
-
-      return url.toString()
-    },
-  },
+  }
 })
